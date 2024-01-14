423048f3b4c5b82451abb4429ed1cbb7176f8725
Aryan Jagarwal <68804795+aryanjagarwal@users.noreply.github.com>
2024-01-14 12:32:52 +0530

    components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Login from "./pages/Login"
+import Navbar from "./components/Navbar"
+import ChatRoom from "./pages/ChatRoom"
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    {/* <Login/> */}
+    <ChatRoom/>
+    </>
+  )
+}
+
+export default App
